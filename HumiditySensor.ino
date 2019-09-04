2c0824032edf067a187cdb1c7e821ab41dbe7d5e
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-04 12:01:01 +0530

    Create HumiditySensor.ino

---
+++ HumiditySensor.ino
@@ -0,0 +1,9 @@
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(1000);
+}
